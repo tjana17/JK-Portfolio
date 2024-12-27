6ca2976b04523ce07aea074221a48e9f01f75b81
JKannan17 <j.kannan@cognetiklabs.com>
2024-12-27 18:29:01 +0530

    Initial Commit

---
+++ JK-Portfolio/JK_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  JK_PortfolioApp.swift
+//  JK-Portfolio
+//
+//  Created by Janarthanan Kannan on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct JK_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
